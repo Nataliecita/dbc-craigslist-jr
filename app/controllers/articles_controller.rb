efb8f9b10b020801183269c69358bc9112fcf714
Natalie Ruiz <email": null>
2017-05-29 14:37:56 -0400

    make sure new article route works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,11 @@ class ArticlesController < ApplicationController
     @category = Category.find(params[:category_id])
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+  end
 end
